b7575d7305d30ea38484dfbc8adfe695e225c783
zzghost <zhouliu712@gmail.com>
2017-10-26 10:21:26 +0800

    Update

--- chapter03/tex_Quotes.java
+++ chapter03/tex_Quotes.java
@@ -5,27 +5,27 @@ import java.util.Scanner;
  * 
  */
 public class tex_Quotes {
-	public static void main(String[] args){
-		//input
-		Scanner in = new Scanner(System.in);
-		int n = 0;
-		while(in.hasNext()){
-			String input = in.nextLine();
-			char[] alpha = input.toCharArray();
-			for(int i = 0; i < input.length(); i++){
-				if(alpha[i] == '\"'){
-					if(n % 2 == 0){
-						System.out.print("``");
-					}
-					else{
-						System.out.print("''");
-					}
-					n++;
-				}
-				else
-					System.out.print(alpha[i]);
-			}
-			System.out.println();
-		}
-	}
+    public static void main(String[] args){
+        //input
+        Scanner in = new Scanner(System.in);
+        int n = 0;
+        while(in.hasNext()){
+            String input = in.nextLine();
+            char[] alpha = input.toCharArray();
+            for(int i = 0; i < input.length(); i++){
+                if(alpha[i] == '\"'){
+                    if(n % 2 == 0){
+                        System.out.print("``");
+                    }
+                    else{
+                        System.out.print("''");
+                    }
+                    n++;
+                }
+                else
+                    System.out.print(alpha[i]);
+            }
+            System.out.println();
+        }
+    }
 }
