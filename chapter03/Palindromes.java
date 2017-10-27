eb0e8512bb82240e2481fed1cbbcc80f78e0ea03
zzghost <zhouliu712@gmail.com>
2017-10-27 20:50:08 +0800

    Palindromes.java

--- chapter03/Palindromes.java
+++ chapter03/Palindromes.java
@@ -1,39 +1,26 @@
-import java.io.BufferedInputStream;
-import java.util.HashMap;
 import java.util.Scanner;
 
 public class Palindromes {
 	public static void main(String[] args) {
-		Scanner in = new Scanner(new BufferedInputStream(System.in));
+		Scanner in = new Scanner(System.in);
 		while(in.hasNext()){
 			String input = in.nextLine();
-			char[] alpha = input.toCharArray();
-			int size = alpha.length;
+			char[] alpha = input.trim().toCharArray();
+			int size = input.length();
 			int p = 1, m = 1;
-			HashMap<Character, Character> map = new HashMap<Character, Character>();
-			map.put('A', 'A');map.put('E', '3');map.put('H', 'H');
-			map.put('I', 'I');map.put('J', 'L');map.put('L', 'J');
-			map.put('M', 'M');map.put('O', 'O');map.put('S', '2');
-			map.put('T', 'T');map.put('U', 'U');map.put('V', 'V');
-			map.put('W', 'W');map.put('X', 'X');map.put('Y', 'Y');
-			map.put('Z', '5');map.put('1', '1');map.put('2', 'S');
-			map.put('3', 'E');map.put('5', 'Z');map.put('8', '8');
-			for(int i = 0; i < size/ 2; i++)
-				if(alpha[i] != alpha[size - i - 1]){
+			char[] REVERSE = {'A', ' ', ' ', ' ', '3', ' ', ' ', 'H', 'I', 'L', ' ', 
+					'J', 'M', ' ', 'O', ' ', ' ', ' ', '2', 'T', 'U', 'V', 'W', 'X', 'Y', '5', 
+					'1', 'S', 'E', ' ', 'Z', ' ', ' ', '8', ' '};
+			for(int i = 0; i < (size + 1) / 2; i++){
+				if(alpha[i] != alpha[size - i - 1])
 					p = 0;
-					break;
-				}
-			for(int i = 0; i <= size / 2; i++){
-				Character ch = (Character)map.get(map.get(alpha[size - i - 1]));
-				if(ch == null || alpha[i] != (Character)map.get(ch))
+				char ch = (alpha[i] >= 'A' && alpha[i] <= 'Z') ? REVERSE[alpha[i] - 'A'] : REVERSE[alpha[i] - '0' + 25];
+				if(ch != alpha[size - i - 1])
 					m = 0;
 			}
-			switch(2 * m + p){
-				case 0 : System.out.println(input + " -- is not a palindrome.");break;
-				case 1 : System.out.println(input + " -- is a regular palindrome.");break;
-				case 2 : System.out.println(input + " -- is a mirrored string.");break;
-				case 3 : System.out.println(input + " -- is a mirrored palindrome.");break;
-			}
+			final String[] MESG = {" -- is not a palindrome.", " -- is a regular palindrome.", 
+					" -- is a mirrored string.", " -- is a mirrored palindrome."};
+			System.out.println(input + MESG[2 * m + p]);
 			System.out.println();
 		}
 	}
